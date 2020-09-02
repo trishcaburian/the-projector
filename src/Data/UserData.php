d3fb627e8eb7d3d0f47bd458c4429d82fc8787ae
trishcaburian <p.caburian0311@gmail.com>
2020-09-02 23:04:36 +0800

    added NotBlank validation constraint for both username and password

--- src/Data/UserData.php
+++ src/Data/UserData.php
@@ -9,12 +9,17 @@ class UserData
      * @Assert\Email(
      *      message = "{{ value }} is not a valid username."
      * )
+     * 
      * @Assert\Length(
      *      min = 5,
      *      max = 200,
      *      minMessage = "The username cannot be less than {{ limit }} characters long.",
      *      maxMessage = "The username cannot be more than {{ limit }} characters long.",
      *      allowEmptyString = false
+     * )
+     * 
+     * @Assert\NotBlank(
+     *      message = "The username cannot be blank"
      * )
      */
     public $username;
@@ -26,6 +31,10 @@ class UserData
      *      minMessage = "The password cannot be less than {{ limit }} characters long.",
      *      maxMessage = "The password cannot be more than {{ limit }} characters long.",
      *      allowEmptyString = false
+     * )
+     * 
+     * @Assert\NotBlank(
+     *      message = "The password cannot be blank"
      * )
      */
     public $password;
