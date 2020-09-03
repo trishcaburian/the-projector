077e415b22646cd20dd03291e83d2b4d97d12eba
trishcaburian <p.caburian0311@gmail.com>
2020-09-03 18:22:16 +0800

    clean PersonsController

--- src/Controller/PersonsController.php
+++ src/Controller/PersonsController.php
@@ -1,16 +1,13 @@
 <?php
 namespace App\Controller;
 
-use App\Data\PersonData;
-use App\Model\PersonModel;
-use App\Model\ProjectInputModel;
-use App\Services\PersonService;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PersonsController extends AbstractController
@@ -29,24 +26,6 @@ class PersonsController extends AbstractController
      */
     public function renderCreatePerson()
     {
-        $person_model = new PersonModel($this->entityManager);
-        $result = $person_model->getUserData();
-        return $this->render('persons/create.html.twig', $result);
-    }
-
-    /**
-     * @Route("/persons/create", name="process_person", methods={"POST"})
-     */
-    public function processPerson(Request $request)
-    {
-        $project_input_model = new ProjectInputModel($this->entityManager, $this->validator);
-        $person_service = new PersonService();
-        $result = $project_input_model->createPerson($person_service->generatePersonData($request));
-
-        if ($result->isValid) {
-            return $this->redirect($this->generateUrl('homepage'));
-        } else {
-            return new Response($this->renderView('persons/create.html.twig', ['errors' => $result->result_list]));
-        }
+        return $this->render('persons/create.html.twig');
     }
 }
